6f14f526416a81731e1c02675aa08a2cc366deeb
Ahmed LIMEM <ahmed.limem@etudiant-enit.utm.tn>
2022-10-01 14:48:15 +0200

    Update setup.py
    add author contact

--- distributions_python_package/setup.py
+++ distributions_python_package/setup.py
@@ -4,4 +4,6 @@ setup(name='distributions',
       version='0.1',
       description='Gaussian and binomial distributions',
       packages=['distributions'],
+      author='Ahmed Limem',
+      author_email='ahmed.limem@etudiant-enit.utm.tn',
       zip_safe=False)
